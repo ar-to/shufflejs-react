562aaf9cad28e3654495cdcc2d40525cb90c54a9
ar-to <aricode00@gmail.com>
2020-01-28 21:22:59 -0800

    updated example and test file to fix test errors

--- examples/example.js
+++ examples/example.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-console */
 const defaultAwesomeFunction = require('../lib').default;
-const { awesomeFunction } = require('../lib');
+const { awesomeFunction, SortFilter } = require('../lib');
 
 const defaultVal = defaultAwesomeFunction('Dinesh');
 const val = awesomeFunction();
@@ -9,3 +9,14 @@ const val = awesomeFunction();
 console.log(defaultVal);
 // val === 'I am just an Awesome Function'
 console.log(val);
+/**
+ * [Function: SortFilter] {
+  propTypes: {
+    defaultSort: [Function: bound checkType],
+    taxonomies: [Function: bound checkType],
+    itemSelector: [Function: bound checkType],
+    children: [Function: bound checkType]
+  }
+}
+ */
+console.log(SortFilter);
